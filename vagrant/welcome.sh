2046f585263c7733622090e2ab02b05c3cbff379
Daniel Hansson <enoch85@gmail.com>
2015-08-10 20:41:10 +0200

    some final touches

--- vagrant/welcome.sh
+++ vagrant/welcome.sh
@@ -5,7 +5,7 @@ ls -la /vagrant
 echo --------------------
 
 # Passwords
-ocpassw=admin
+ocuser=admin
 ocpassw=admin
 rootpassw=root
 # Get current address
@@ -22,19 +22,20 @@ cat << WELCOME > "$welcome_sh"
 #!/bin/bash
 clear 
 cat << EOM
--------------------------------------------------------------------------
+---------------------------------------------------------------------------
 |    Welcome to ownCloud, your server is now ready!                       |
-|                                                                 |
-|    Please go to $ADDRESS/owncloud to access your ownCloud        |
-|    Your ownCloud admin account is: login: $ocuser passwd: $ocpassw              |
-|    Your Linux root password is: $rootpassw                      |
-|                                                                 |
+|                                                                         |
+|    Please go to $ADDRESS/owncloud to access your ownCloud               |
+|    Your ownCloud admin account is: login: $ocuser passwd: $ocpassw      |
+|    Your Linux root password is: $rootpassw                              |
+|                                                                         |
 |    More information in the documentation at: http://doc.owncloud.org/   |
 |                                                                         |
--------------------------------------------------------------------------
+|    To run the setup-script, please type: sudo -i, or login as root      |
+---------------------------------------------------------------------------
 EOM
 
 WELCOME
 
-# Put welcome.sh in ~./profile
+# Put welcome.sh in ~/.profile
 sed -i '$a bash /var/scripts/welcome.sh' /home/vagrant/.profile
