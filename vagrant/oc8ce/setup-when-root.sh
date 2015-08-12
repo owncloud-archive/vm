f98a0cb3639af466d3549476eccc92baec6437e7
Daniel Hansson <enoch85@gmail.com>
2015-08-12 20:14:29 +0200

    Tested and working!
    No need for changing password on ownCloud as the user set it up from start wit the password he/she wants
    
    Everything is working as expected.

--- vagrant/oc8ce/setup-when-root.sh
+++ vagrant/oc8ce/setup-when-root.sh
@@ -13,7 +13,7 @@ cat << EOMSTART
 |                                                               |                                                                    
 |   - Change keyboard setup                                     |                                                                 
 |   - Change timezone                                           |                                                                 
-|   - Set new passwords to xUbuntu (Vagrant user) and ownCloud  |                                                                 
+|   - Set new passwords to xUbuntu (Vagrant user)               |                                                                 
 |   - Activate a Virtual Host for your ownCloud install         |                                                                 
 |   - Activate Self-signed SSL (self-signed-ssl.conf)           |
 -----------------------------------------------------------------
