9be096a6f1a2e2ab57efba6a37a2212e9208f1f5
Daniel Hansson <enoch85@gmail.com>
2015-08-12 22:30:00 +0200

    change back to the setup-when-root
    Add: 
    - Change trusted domain
    - Add memcached and the correct values in config.php
    - Set secure permissions 
    - rm owncloud.log
    
    Question is, should we do it here  or in build-ubunutu.sh?

--- vagrant/oc8ce/setup-when-root.sh
+++ vagrant/oc8ce/setup-when-root.sh
@@ -1,46 +1,77 @@
-#!/bin/bash
+ #!/bin/bash
+ clear 
+-cat << EOMSTART
+ +---------------------------------------------------------------+
+-|   This script will do the final setup for you                 |
+-|                                                               |                                                                    
+-|   - Change keyboard setup (current is US)                     |                                                                 
+-|   - Change timezone                                           |                                                                 
+-|   - Set new passwords to xUbuntu and ownCloud (user: $user)   |                                                                 
+-|   - Activate Self-signed SSL (self-signed-ssl.conf)           |
+ +---------------------------------------------------------------+
+-
+-The script will begin in 10 seconds...
+-EOMSTART
+-sleep 10
+-clear
+-
+-# Set keyboard layout
+-echo "Current keyboard layout is US"
+-echo "You must change keyboard layout to your language"
+-echo -e "\e[32m"
+-read -p "Press any key to change keyboard layout... " -n1 -s
+-echo -e "\e[0m"
+-dpkg-reconfigure keyboard-configuration 
+-echo
+-clear
+-
+-# Change Timezone
+-echo "Current Timezone is UTC"
+-echo "You must change timezone to your timezone"
+-echo -e "\e[32m"
+-read -p "Press any key to change timezone... " -n1 -s
+-echo -e "\e[0m"
+-dpkg-reconfigure tzdata
+-echo
+-sleep 3
+-clear
+-
+-# Change password
+-echo -e "\e[0m"
+-echo "For better security, change the Ubuntu password for [admin]"
+-echo "The current password is [$password]"
+-echo -e "\e[32m"
+-read -p "Press any key to change password for Linux... " -n1 -s
+-echo -e "\e[0m"
+-sudo passwd admin
+-echo
+-clear 
 
-# Need to fix:
-# - ADRESS is showing wrong IP
-# - Maybe combine this script with setup_when_root.sh?
-
-echo scripts in /vagrant:
-ls -la /vagrant
-echo --------------------
-
-# Passwords
-ocuser=admin
-ocpassw=admin
-rootpassw=admin
-# Get current address
-IFACE="eth0"
-IFCONFIG="/sbin/ifconfig"
-ADRESS=$($IFCONFIG $IFACE | awk -F'[: ]+' '/\<inet\>/ {print $4; exit}')
-# The Welcome message
-welcome_sh="/var/scripts/welcome.sh"
-mkdir -p /var/scripts
-
-# Create welcome.sh and put that in ~/.profile
-set -x
-cat << WELCOME > "$welcome_sh"
-#!/bin/bash
-clear 
-cat << EOM
----------------------------------------------------------------------------
-|    Welcome to ownCloud, your server is now ready!                       |
-|                                                                         |
-|    Please go to $ADRESS/owncloud to access your ownCloud              |
-|    Your ownCloud admin account is: login: $ocuser passwd: $ocpassw		  |
-|    Your Linux root password is: $rootpassw					  |
-|                                                                         |
-|    More information in the documentation at: http://doc.owncloud.org/   |
-|                                                                         |
-|    To run the setup-script, please type: sudo -i, or login as root      |
----------------------------------------------------------------------------
-EOM
-
-WELCOME
-
-# Put welcome.sh in ~/.profile
-# REALLY?
-# sed -i '$a bash /var/scripts/welcome.sh' /home/vagrant/.profile
+echo -e "\e[0m"
+echo "For better security, change the ownCloud password for [$user]"
+echo "The current password is [$password]"
+echo -e "\e[32m"
+read -p "Press any key to change password for ownCloud... " -n1 -s
+echo -e "\e[0m"
+sudo -u www-data php /var/www/owncloud/occ user:resetpassword $password
+echo
+-
+-cat << EOMFINISH
+-
+ +-------------------------------------------------+
+-|   Success! You have now done the final setup.   |
+-|   The system will now reboot...                 |
+ +-------------------------------------------------+
+-
+-EOMFINISH
+-sleep 4
+-# Remove the script so that it won't run every time the user becomes root
+-rm /var/scripts/setup-when-root.sh
+-
+-# Reboot
+-read -p "Press any key to reboot..." -n1 -s
+-echo -e "\e[0m"
+-echo
+-reboot
+-
+-SETUP
