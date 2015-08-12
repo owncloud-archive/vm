93c46da6325be0390c5e7849f4cd69f8a4493f5e
Daniel Hansson <enoch85@gmail.com>
2015-08-12 14:16:21 +0200

    Update owncloud_update.sh

--- enoch85-testing/ubuntu/owncloud_update.sh
+++ enoch85-testing/ubuntu/owncloud_update.sh
@@ -3,10 +3,6 @@
 ## Tech and Me ## - 2015, https://www.en0ch.se/
 #
 
-# Cleanup unused packages
-apt-get autoremove -y 
-apt-get autoclean 
-
 # System Upgrade
 apt-get update 
 aptitude full-upgrade -y 
@@ -23,6 +19,10 @@ sudo -u www-data php /var/www/owncloud/occ app:enable external
 
 # Set secure permissions to ownCloud
 bash /var/scripts/setup_secure_permissions_owncloud.sh
+
+# Cleanup unused packages
+apt-get autoremove -y 
+apt-get autoclean
 
 # Write to log
 echo "OWNCLOUD UPDATE success-`date +"%Y%m%d"`  " >> /var/log/cronjobs_success.log
