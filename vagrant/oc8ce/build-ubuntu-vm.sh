90b03afd64dc1d5831ae372c8a758be6ea19937a
Jürgen Weigert <jnweiger@gmail.com>
2015-08-31 09:56:38 +0200

    fixing the repo-urls. Less error messages during build.

--- vagrant/oc8ce/build-ubuntu-vm.sh
+++ vagrant/oc8ce/build-ubuntu-vm.sh
@@ -33,10 +33,11 @@ vmBoxUrl=https://vagrantcloud.com/ubuntu/boxes/trusty64/versions/14.04/providers
 # vmBoxUrl=https://vagrantcloud.com/ubuntu/boxes/vivid64/versions/20150722.0.0/providers/virtualbox.box
 
 OBS_REPO=$OBS_MIRRORS/$(echo $OBS_PROJECT | sed -e 's@:@:/@g')/$buildPlatform
-OBS_REPO_APCU=$OBS_MIRRORS/isv:ownCloud:devel/$buildPlatform
-OBS_REPO_PROXY=$OBS_MIRRORS/isv:ownCloud:8.1:testing:merged/$buildPlatform
+OBS_REPO_APCU=$OBS_MIRRORS/isv:/ownCloud:/devel/$buildPlatform
+OBS_REPO_PROXY=$OBS_MIRRORS/isv:/ownCloud:/community:/8.1:/testing:/merged/$buildPlatform
 ocVersion=$(curl -s -L $OBS_REPO/Packages | grep -a1 'Package: owncloud$' | grep Version: | head -n 1 | sed -e 's/Version: /owncloud-/')
-# ocVersion=ownCloud-8.1.0-6
+# ocVersion=owncloud-8.1.0-6
+# ocVersion=owncloud-8.1.2~RC1-6.1
 test -z "$ocVersion" && { echo "ERROR: Cannot find owncloud version in $OBS_REPO/Packages -- Try again later"; exit 1; }
 ocVersion=$(echo $ocVersion | tr '~' -)
 vmName=$(echo $ocVersion | sed -e 's/owncloud/oc8ce/')
