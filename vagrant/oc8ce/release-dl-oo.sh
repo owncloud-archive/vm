f4d1d6ad9fc31f5bed777ceac3d46606121363a8
Jürgen Weigert <jnweiger@gmail.com>
2015-09-08 18:49:10 +0200

    a script to upload to download.owncloud.org

---
+++ vagrant/oc8ce/release-dl-oo.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+#
+# release-dl-oo -- push released files into download.owncloud.org
+# (c) Copyright 2015 jw@owncloud.com
+#
+
+## testing releases go here:
+dest=upload@dl-oo.owncloud.org:w/testing/vm/
+## production releases go here:
+# dest=upload@dl-oo.owncloud.org:w/production/vm/
+
+echo uploading to $dest ...
+set -x
+sleep 3
+sleep 2
+sleep 1
+
+imgdir=$(dirname $0)/img
+for file in $imgdir/*.zip; do
+  chmod 0644 $file
+  rsync --progress --append-verify $file $dest/
+done
+
