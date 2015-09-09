fd1fc33ca38e44420c50ea75e4d9abdc6d779dbd
Jürgen Weigert <juewei@fabfolk.com>
2015-09-09 21:31:06 +0200

    pulled all together in run.sh

--- vagrant/run.sh
+++ vagrant/run.sh
@@ -3,11 +3,12 @@
 # time sh -x oc8ce/build-ubuntu-vm.sh isv:ownCloud:community:8.1:testing:merged
 
 #### use this for production:
-DEBUG=false sh oc8ce/build-ubuntu-vm.sh isv:ownCloud:community:8.1
+time DEBUG=false sh oc8ce/build-ubuntu-vm.sh isv:ownCloud:community:8.1
 
 # use this variable to run the install-additional-apps.sh script.
 # INSTALL_ADDITIONAL_APPS=1 sh ...  
 
-# TODO: * run the converters to produce all neede formats
-# TODO: * find a place where to publish. on http://download.owncloud.org/community or on https://github.com/owncloud/vm/releases
 
+#### upload stuff. First github, which is fast, then our own serrver.
+oc8ce/release-github.sh
+oc8ce/release-dl-oo.sh
