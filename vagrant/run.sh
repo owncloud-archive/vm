3617b2ee2cc8764b442d9689c771f4d20293f100
Jürgen Weigert <juewei@fabfolk.com>
2015-09-16 17:15:51 +0200

    production builds come from non-testing.

--- vagrant/run.sh
+++ vagrant/run.sh
@@ -5,15 +5,15 @@
 # time sh -x oc8ce/build-ubuntu-vm.sh isv:ownCloud:community:8.1:testing $1
 
 #### use this for production:
-DEBUG=false time sh oc8ce/build-ubuntu-vm.sh isv:ownCloud:community:8.1:testing $1
+DEBUG=false time sh oc8ce/build-ubuntu-vm.sh isv:ownCloud:community:8.1 $1
 
 # use this variable to run the install-additional-apps.sh script.
 # INSTALL_ADDITIONAL_APPS=1 sh ...  
 
 
 #### upload stuff. First github, which is fast, then our own serrver.
-echo now run oc8ce/release-github.sh
-echo now run oc8ce/release-dl-oo.sh
+oc8ce/release-github.sh
+oc8ce/release-dl-oo.sh
 
 echo "If this is an official release, update the version number in"
 echo "https://github.com/owncloud/owncloud.org/blob/officialvm/strings.php"
