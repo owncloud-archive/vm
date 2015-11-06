9cb946762186fabc1d387375a52c5e1c200ec5bf
Daniel Hansson <enoch85@gmail.com>
2015-11-07 00:14:35 +0100

    typo

--- vagrant/oc8ce/set-trusted-domain.sh
+++ vagrant/oc8ce/set-trusted-domain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # only set trusted domian once
-# then delete this file from in check.init.sh after the script is run to avoid new config every time the VM boots
+# then delete this file in check.init.sh after the script is run to avoid new config every time the VM boots
 php /var/scripts/update-config.php $oc/config/config.php 'trusted_domains[]' localhost ${ADDRESSES[@]} $(hostname) $(hostname --fqdn)
 php /var/scripts/update-config.php $oc/config/config.php overwrite.cli.url https://$ADDRESS/owncloud
