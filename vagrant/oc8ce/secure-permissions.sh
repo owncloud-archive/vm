b2b1eb1b6ca20154dd442ab215e5e051163702db
Daniel Hansson <enoch85@gmail.com>
2016-02-14 00:21:00 +0100

    Update secure-permissions.sh

--- vagrant/oc8ce/secure-permissions.sh
+++ vagrant/oc8ce/secure-permissions.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 ocpath='/var/www/owncloud'
 htuser='www-data'
+htgroup='www-data'
+rootuser='root'
 
+printf "Creating possible missing Directories\n"
+mkdir -p $ocpath/data
+mkdir -p $ocpath/assets
+
+printf "chmod Files and Directories\n"
 find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
 find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
 
-chown -R root:${htuser} ${ocpath}/
-chown -R ${htuser}:${htuser} ${ocpath}/apps/
-chown -R ${htuser}:${htuser} ${ocpath}/config/
-chown -R ${htuser}:${htuser} ${ocpath}/data/
+printf "chown Directories\n"
+chown -R ${rootuser}:${htgroup} ${ocpath}/
+chown -R ${htuser}:${htgroup} ${ocpath}/apps/
+chown -R ${htuser}:${htgroup} ${ocpath}/config/
+chown -R ${htuser}:${htgroup} ${ocpath}/data/
+chown -R ${htuser}:${htgroup} ${ocpath}/themes/
+chown -R ${htuser}:${htgroup} ${ocpath}/assets/
 
-chown root:${htuser} ${ocpath}/.htaccess
-chown root:${htuser} ${ocpath}/data/.htaccess
+chmod +x ${ocpath}/occ
 
-chmod 0644 ${ocpath}/.htaccess
-chmod 0644 ${ocpath}/data/.htaccess
-
-exit 0
+printf "chmod/chown .htaccess\n"
+if [ -f ${ocpath}/.htaccess ]
+ then
+  chmod 0644 ${ocpath}/.htaccess
+  chown ${rootuser}:${htgroup} ${ocpath}/.htaccess
+fi
+if [ -f ${ocpath}/data/.htaccess ]
+ then
+  chmod 0644 ${ocpath}/data/.htaccess
+  chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
+fi
