dc73227f643a33519a9472426e13f211bcabdee4
Juergen Weigert <jw@owncloud.com>
2016-09-01 13:00:01 +0200

    no more password clear text printing. added php-ldap module.

--- vagrant/oc9ce/change_pass_admin.sh
+++ vagrant/oc9ce/change_pass_admin.sh
@@ -5,18 +5,9 @@ cred_file=$cred_file_dir/init-credentials.sh
 
 echo "For better security, change the Ubuntu password for [admin]"
 echo -n "Enter your new password for admin here:"; stty -echo; read passwd1; stty echo; echo
+echo -n "Enter password again:                  "; stty -echo; read passwd2; stty echo; echo
 echo
-echo -e "Your new password is:\e[32m$passwd1\e[0m"
-echo
-function ask_yes_or_no() {
-        read -p "$1 ([y]es or [N]o): "
-        case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
-        y|yes) echo "yes" ;;
-        *)     echo "no" ;;
-    esac
-}
-if [[ "yes" == $(ask_yes_or_no "Is this correct?") ]]
-then
+if [[ "$passwd1" == "$passwd2" ]]; then
     echo -e "admin:$passwd1" | chpasswd
     echo -e "user=admin\npassword=$passwd1" > $cred_file
     echo -e "\e[32mPassword changed successfully!\e[0m"
