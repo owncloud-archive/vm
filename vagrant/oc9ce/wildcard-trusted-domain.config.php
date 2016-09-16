687dd9fd7412216ddf3a0a865c68bf89c80afde7
Juergen Weigert <jw@owncloud.com>
2016-09-16 15:17:50 +0200

    use localhost if _SERVER is unset. Needed with occ...

--- vagrant/oc9ce/wildcard-trusted-domain.config.php
+++ vagrant/oc9ce/wildcard-trusted-domain.config.php
@@ -1,2 +1,11 @@
-<?php $CONFIG = array ( 'trusted_domains' => array ( $_SERVER['HTTP_HOST'] ) );
+<?php
+if (isset($_SERVER['HTTP_HOST']))
+{
+  $CONFIG = array ( 'trusted_domains' => array ( 'localhost', $_SERVER['HTTP_HOST'], ), );
+}
+else
+{
+  $CONFIG = array ( 'trusted_domains' => array ( 'localhost', ) );
+}
+
 
