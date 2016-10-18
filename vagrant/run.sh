dd1b84c3778fae36a16940b714cf4758d827830b
Juergen Weigert <jw@owncloud.com>
2016-10-18 14:12:16 +0200

    idea for adding an app.

--- vagrant/run.sh
+++ vagrant/run.sh
@@ -2,37 +2,21 @@
 # it will wait until that version number appears in the repo.
 #
 
-if [ -z "$GITHUB_TOKEN" ]; then
-  echo "Environment variable GITHUB_TOKEN not set. See https://github.com/settings/tokens"
-  echo "You will have to call oc9ce/release-github.sh later manually. Press ENTER to continue"
-  read a
-fi
-
 #### use this for testing:
 # override DOO_MIRRORS in build-ubuntu-vm.sh
-export OC_REPO_URL=http://owncloud:*PASSWORD*@obs.int.owncloud.com:83/
-# time sh -x oc8ce/build-ubuntu-vm.sh ce:8.2:testing $1
-
-#### use this for production:
-# DEBUG=false time sh oc8ce/build-ubuntu-vm.sh ce:8.2 $1
+#export OC_REPO_URL=http://owncloud:*PASSWORD*@obs.int.owncloud.com:83/
+#
+## can be a comma separated list...
+export OC_APPS_URL=https://apps.owncloud.com/CONTENT/content-files/155100-appvncsafe.zip
 
 #### use this for production:
 DEBUG=false time sh oc9ce/build-ubuntu-vm.sh ce:9.1 $1
 
-# use this variable to run the install-additional-apps.sh script.
-# INSTALL_ADDITIONAL_APPS=1 sh ...  
-
-
 #### upload stuff. 
-## first our own server, even if slower, this is what people are waiting for.
 ## use this for testing
-#oc8ce/release-dl-oo.sh testing/vm
+# oc9ce/release-dl-oo.sh testing/vm
 ## use this for production
-#oc8ce/release-dl-oo.sh production/vm
 oc9ce/release-dl-oo.sh production/vm
-
-## second github. This is likely to error out with 502 bad gateway.
-# oc8ce/release-github.sh
 
 echo "If this is an official release, update the version number in"
 echo "https://github.com/owncloud/owncloud.org/blob/master/strings.php"
